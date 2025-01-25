9a78fbbf70c81b5a59c0f5fcb112c80f6a5372a5
boonzzzzz <boon19112550@gmail.com>
2025-01-25 15:35:01 +0700

    deadscene

--- Assets/Script/Death.cs
+++ Assets/Script/Death.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem.Processors;
 using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
     public ParticleSystem collisionEffect; // ใส่ Particle System ที่ต้องการเล่น
-
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         // ตรวจสอบว่าชนกับ Fish หรือไม่
@@ -13,11 +14,13 @@ public class Death : MonoBehaviour
             // เล่น Particle Effect ที่ตำแหน่งของ Player
             ParticleSystem effect = Instantiate(collisionEffect, transform.position, Quaternion.identity);
             effect.Play(); // เริ่มเล่น Particle Effect
-
+            
             // ปิดการทำงานของ Player ชั่วคราว
             gameObject.SetActive(false);
-
+            playerscript.dead++;
             // รอจน Particle เล่นเสร็จ
+            playerscript.ready = true;
+            Debug.Log(playerscript.dead);
             StartCoroutine(WaitForParticleAndRestart(effect));
         }
     }
@@ -29,8 +32,9 @@ public class Death : MonoBehaviour
         {
             yield return null; // รอเฟรมถัดไป
         }
-
+        
         // รีเซ็ต Scene หลัง Particle Effect เล่นเสร็จ
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    
 }
