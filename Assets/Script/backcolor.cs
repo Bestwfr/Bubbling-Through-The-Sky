57cadbb446c3c0f52365178d5b03e7f329fe0112
Best <best.xp345@gmail.com>
2025-01-25 21:18:01 +0700

    dead scence

--- Assets/Script/backcolor.cs
+++ Assets/Script/backcolor.cs
@@ -3,7 +3,13 @@ using UnityEngine.UI;
 
 public class backcolor : MonoBehaviour
 {
-    
+    void Update()
+{
+    if (Input.GetKeyDown(KeyCode.K)) // Press "K" to simulate player death
+    {
+        FindObjectOfType<BackgroundMusic>().PlayDeathSound();
+    }
+}
 
 
 }
