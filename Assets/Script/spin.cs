cde6ee167983dec0cd56def536aa76dfffb724ee
Ninoyahoo <kongxxddd@gmail.com>
2025-01-26 02:29:15 +0700

    mainmenu2.0

--- Assets/Script/spin.cs
+++ Assets/Script/spin.cs
@@ -1,13 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public Vector3 Angle = Vector3.one;
+    public Vector3 Angle = Vector3.one; // องศาการหมุน
 
     void Update()
     {
-        transform.Rotate(Angle * 24 * Time.deltaTime);
+        transform.Rotate(Angle * 24 * Time.deltaTime); // หมุนวัตถุตามอัตราเร่ง
     }
 }
